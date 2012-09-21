55b64c9086e7c65ab8580aecc9052bb321b3d2db
raymondlesley@gmail.com <raymondlesley@gmail.com@1644ad1f-aa82-55a9-4f2f-16511c5ce74a>
2012-09-21 10:50:43 +0000

    Added "About" menu item and stub menu handler

--- src/uk/co/thelesleys/simple_shopping_list_android/MainActivity.java
+++ src/uk/co/thelesleys/simple_shopping_list_android/MainActivity.java
@@ -6,7 +6,22 @@ import android.util.Log;
 import android.view.Menu;
 // import android.view.MenuItem;
 // import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
+// TODO: work through http://www.vogella.com/articles/AndroidListView/article.html
+//       (or similar)
+//       as the basis of the shopping list App
+
+/**
+ * @author lesleyr
+ * @version 1
+ *
+ */
+
+/**
+ * Main Activity
+ *
+ */
 public class MainActivity extends Activity {
 
     @Override
@@ -20,9 +35,25 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+		Log.i("MainActivity", "onCreateOptionsMenu()");
+
+		getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.i("MainActivity", "onOptionsItemSelected(" + item.getTitle() + ")");
+
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			Log.i("MainActivity", "Menu item: Settings");
+			return true;  // handled
+		case R.id.menu_about:
+			Log.i("MainActivity", "Menu item: About");
+			return true;  // handled
+		}
+		return super.onOptionsItemSelected(item);
+	}
     
 }
