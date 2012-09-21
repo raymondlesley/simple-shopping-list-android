25c648041e35cc9878ee4cfe5d80dd73d4e54624
raymondlesley@gmail.com <raymondlesley@gmail.com@1644ad1f-aa82-55a9-4f2f-16511c5ce74a>
2012-09-21 12:31:34 +0000

    Replace dummy text view with placeholder list view ... including dummy menu handlers to demo

--- src/uk/co/thelesleys/simple_shopping_list_android/MainActivity.java
+++ src/uk/co/thelesleys/simple_shopping_list_android/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 // import android.view.MenuItem;
 // import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 // TODO: work through http://www.vogella.com/articles/AndroidListView/article.html
 //       (or similar)
@@ -48,9 +50,33 @@ public class MainActivity extends Activity {
 		switch (item.getItemId()) {
 		case R.id.menu_settings:
 			Log.i("MainActivity", "Menu item: Settings");
+
+			{
+				// TODO: for RAD only - dummy implementation!
+		        ListView listView = (ListView)findViewById(R.id.main_list);
+		        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		          "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		          "Linux", "OS/2" };
+		        // First parameter - Context
+		        // Second parameter - Layout for the row
+		        // Third parameter - ID of the TextView to which the data is written
+		        // Forth - the Array of data
+		        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				          android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		        // Assign adapter to ListView
+		        listView.setAdapter(adapter); 
+			}
+
 			return true;  // handled
 		case R.id.menu_about:
 			Log.i("MainActivity", "Menu item: About");
+
+			{
+		        // TODO: for RAD only - dummy implementation!
+		        ListView listView = (ListView)findViewById(R.id.main_list);
+		        listView.setAdapter(null);
+			}
+
 			return true;  // handled
 		}
 		return super.onOptionsItemSelected(item);
